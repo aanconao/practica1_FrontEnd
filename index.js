fc34f7007594627f09066eb4a4df5459b33104fb
Alan Ancona Ortiz <167805532+aanconao@users.noreply.github.com>
2025-02-19 08:39:35 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+document.addEventListener("click", MyFunction);
+
+function MyFunction() {
+  document.getElementById("prueba").innerHTML =
+    "<ul>FORMACION ACADEMICA : Universidad nebrija</ul>";
+}
